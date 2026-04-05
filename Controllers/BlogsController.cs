537d4ef3112aae07ce471c192501e934d7fc8e9a
irem guler <iguler395@gmail.com>
2026-04-05 13:20:51 +0300

    deneme2

---
+++ Controllers/BlogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogV1.Controllers
+{
+    public class BlogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

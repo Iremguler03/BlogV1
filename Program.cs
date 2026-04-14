44ece9cc15fa9c709d69ee5e6d35faa6828e6649
irem guler <iguler395@gmail.com>
2026-04-14 13:07:01 +0300

    deneme12

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
 using BlogV1.Context;
+using BlogV1.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 //
 builder.WebHost.UseWebRoot("wwwroot");
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BlogDbContext>();
+//ıdentity db context baglantisi
+builder.Services.AddDbContext<BlogIdentityDbContext>(options =>
+{
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
+
+//admin giris yapmadiginda kullanicilari otomatik bir sayfaya yonlendirme
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Blogs/Index"; 
+    });
+
+builder.Services.AddIdentity<BlogIdentityUser, BlogIdentityRole>()
+    .AddEntityFrameworkStores<BlogDbContext>()
+    .AddDefaultTokenProviders();
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
